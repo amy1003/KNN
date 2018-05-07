0b869d39938527e8e0f689b968e44e5da838468f
Chih-Chieh Lee <clee7@caltech.edu>
2018-05-06 21:28:01 -0700

    add knn using pyreclab

--- README.md
+++ README.md
@@ -1,19 +1,48 @@
 # KNN
 
+## Requirement
+
+
 ## Data processing
 1. Place the netflix challenge data into dataset directory.
-    If you already have them elsewhere, create a symbolic link. 
-    In linux this is accomplished by:
+    
+If you already have them elsewhere, create a symbolic link. 
+In linux this is accomplished by (from the root directory):
 
     ```
-    ln -s <path to um/> ./um
-    ln -s <path to mu/> ./mu
+    ln -s <path to um/> ./dataset/um
+    ln -s <path to mu/> ./dataset/mu
     ```
 
 2. Run the preprocessing script to split the data into `training`, 
-   `validation`, `test` and `probe` sets.
+`validation`, `test` and `probe` sets.
 
-    *Use Python 3 to ensure compatibility, although Python 2 should work too.*
+*Use Python 3 to ensure compatibility, although Python 2 should work too.*
+
     ```
     python preprocess.py
     ```
+
+## Train and predict
+
+To train and predict:
+
+    '''
+    python src/KNN.py -t dataset/um/base.dta -q dataset/um/qual.dta -o result/output.dta
+    '''
+
+To test code on UserAvg:
+
+    '''
+    python src/KNN.py -t dataset/um/base.dta -q dataset/um/qual.dta -o result/output.dta --code_test 1
+    '''
+Options:
+* -t (--train) `<File path>`: Required
+    Training data file path.
+* -q (--qual) `<File path>`: Required
+    Qual data file path.
+* -o (--ouput) `<File path>`: Required
+    Output data file path.
+* --code_test  `<int>`: Optional
+    Test code tag. 
+    
---
+++ src/KNN.py
@@ -0,0 +1,47 @@
+
+import pyreclab
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='KNN')
+    parser.add_argument('--train', '-t', required=True, type=str,
+                        help='input training data')
+    parser.add_argument('--qual', '-q', required=True, type=str,
+                        help='input qual data')
+    parser.add_argument('--output', '-o', required=True, type=str,
+                        help='output file path')
+    parser.add_argument('--code_test', default=0, type=int,
+                        help='test code using UserAvg')
+    args = parser.parse_args()
+
+    if (args.test == 0):
+        obj = pyreclab.UserKnn(dataset=args.train,
+                               dlmchar=b'\s',
+                               header=False,
+                               usercol=0,
+                               itemcol=1,
+                               ratingcol=3)
+    else:
+        obj = pyreclab.UserAvg(dataset=args.train,
+                               dlmchar=b'\s',
+                               header=False,
+                               usercol=0,
+                               itemcol=1,
+                               ratingcol=3)
+
+    # Train
+    knn = 10
+    similarity = 'cosine'
+
+    if (args.test == 0):
+        obj.train(knn, similarity)
+    else:
+        obj.train()
+
+    # Predict
+    with open(args.qual, 'r') as qual:
+        with open(args.output, 'w') as pred:
+            for line in qual:
+                x = line.split(' ')
+                r = obj.predict(x[0], x[1])
+                pred.write(str(r) + "\n")
